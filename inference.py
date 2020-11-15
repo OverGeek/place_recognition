695a88541a88c7e7ced370d4a604b07059c49dc1
OverGeek <adhruv1008@gmail.com>
2020-11-15 19:00:05 +0530

    updated place recognition to output best 2 places

--- inference.py
+++ inference.py
@@ -9,7 +9,7 @@ test_X = []
 test_dir = './Complete_dataset/image_with_semantics_objs'
 
 image_names = []
-for img_name in os.listdir(test_dir)[4000:]:
+for img_name in os.listdir(test_dir)[:1000]:
     img = np.load(os.path.join(test_dir, img_name))
     test_X.append(img)
     image_names.append(img_name)
@@ -22,11 +22,11 @@ model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy
 model.load_weights('./Models/saved-model-100-0.94.hdf5')
 
 preds = model.predict(test_X, verbose=True)
-preds = np.argmax(preds, axis=1)
+preds = np.argsort(-1*preds, axis=1)[:, :2]
 
 preds_dict = {}
 for i in range(len(image_names)):
-    preds_dict[image_names[i]] = 'Place_'+str(int(preds[i]))
+    preds_dict[image_names[i]] = ['Place_'+str(int(pred)) for pred in preds[i]]
 
-with open('./preds4.json', 'w') as f:
+with open('./preds1.json', 'w') as f:
     json.dump(preds_dict, f, indent=1)
